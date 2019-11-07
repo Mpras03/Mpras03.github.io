13572bc3a108bce49c70915ac36bfd1d8335b310
Quantum-sc <fernaldiw@gmail.com>
2019-11-07 14:24:55 +0700

    preview image

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -26,3 +26,24 @@ function validasilogin(){
         true;
     }
 }
+
+function tampilkanPreview(gambar,imgpreview){
+    var gb = gambar.files;
+    for (var i = 0; i < gb.length; i++){
+        var gbPreview = gb[i];
+        var imageType = /image.*/;
+        var preview=document.getElementById(imgpreview);
+        var reader = new FileReader();
+        if (gbPreview.type.match(imageType)) {
+            preview.file = gbPreview;
+            reader.onload = (function(element) {
+                return function(e) {
+                    element.src = e.target.result;
+                };
+            })(preview);
+            reader.readAsDataURL(gbPreview);
+        }else{
+            alert("Type file tidak sesuai. Khusus image.");
+        }
+    }
+}
